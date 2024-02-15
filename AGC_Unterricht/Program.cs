50155f8dc9a4a65b579144236e96f9c3f838959b
pbbfa22cpa <pbbfa22cpa@L15>
2024-02-15 09:28:49 +0100

    Projektdateien hinzufügen.

---
+++ AGC_Unterricht/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arztpraxis
+{
+    public class Patient
+    {
+        private string _name;
+
+        // Konstruktor
+        public Patient(string name)
+        {
+            _name = name;
+        }
+
+        // Eigenschaft für den Namen des Patienten
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        // ToString-Methode, um eine lesbare Darstellung des Patienten zu erhalten
+        public override string ToString()
+        {
+            return $"Patient: {_name}";
+        }
+    }
+}
---
+++ Uebung3A-Wartezimmer/Patient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung3A_Wartezimmer
+{
+    internal class Patient
+    {
+        private string _name;
+        public Patient(string name)    
+        { 
+           name = _name;
+        }
+
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+
+    }
+}
